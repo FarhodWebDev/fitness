345c99dc646cb136588b6830859b140068213496
farhodWebDev <yusupovfarhod0902@gmail.com>
2023-09-28 11:38:04 +0500

    add and get users

--- database/src/controllers/user.controller.js
+++ database/src/controllers/user.controller.js
@@ -3,28 +3,42 @@
 import client from "../connectDB.js";
 
 async function getAll(req, res) {
- const users = await client.query("SELECT * FROM users ORDER BY ASC");
- res.send({ message: "hello" });
+ const users = await client.query("SELECT * FROM users");
+ res.status(200).send({...users.rows});
 }
 
 async function postUser(req, res) {
  try {
-  // req.query();
   await client.query(
-   `INSERT INTO fitness.users (${
-    (req.query.first_name),(
-    req.query.last_name,
-    req.query.dob,
-    req.query.pic,
-    req.query.email,
-    req.query.added_day,
-    req.query.phone_number,
-    req.query.information,
-    req.query.role,
-    req.query.toifa,
-    req.query.period,
-    req.query.money)
-   })`
+   `INSERT INTO users (
+   first_name,
+   last_name,
+   dob,
+   pic,
+   email,
+   added_day,
+   phone_number,
+   information,
+   user_role,
+   toifa,
+   user_period,
+   user_money,
+   gender
+   ) values (
+   '${req.query.first_name}',
+   '${req.query.last_name}',
+   '${req.query.dob}',
+   '${req.query.pic}',
+   '${req.query.email}',
+   '${req.query.added_day}',
+   '${req.query.phone_number}',
+   '${req.query.information}',
+   '${req.query.role}',
+   '${req.query.toifa}',
+   '${req.query.period}',
+   '${req.query.money}',
+   '${req.query.gender}'
+   )`
   );
   res.json("user added");
  } catch (e) {
